42c3c6e95de9ad13bff195cca4b4d38144f07244
andrianandraina.mika <mikaely.andrianandraina@esqese.ucly.fr>
2022-04-06 14:20:57 +0200

    projet base + démmmmmmmmmmmmmmmmmmmmmm

---
+++ démineur/main_demineur.py
@@ -0,0 +1,251 @@
+import pygame
+import random
+import pygame_gui
+pygame.init()
+
+bg_color = (192, 192, 192)
+grid_color = (128, 128, 128)
+
+game_width = 10  # changer ses nbr pour incrementer la taille
+game_height = 10  # changer ses nbr pour incrementer la taille
+numMine = 9  # nbr de mines
+grid_size = 32  # taille grille
+border = 16  # bord du haut
+top_border = 100  # bord d-g-b
+display_width = grid_size * game_width + border * 2  # largeur affichage
+display_height = grid_size * game_height + border + top_border  # hauteur affich
+gameDisplay = pygame.display.set_mode((display_width, display_height))  # creation affich
+timer = pygame.time.Clock()  # timer
+pygame.display.set_caption("Démineur")  # defini le commentaire de la page dcp le nom du jeu
+
+# les fichiers que j'importe que j'ai pris sur un git ? (jsp si j'avais le droit)
+#image utilisées mine + drapeur + 1-2-3
+spr_emptyGrid = pygame.image.load("Sprites/empty.png")
+spr_flag = pygame.image.load("Sprites/flag.png")
+spr_grid = pygame.image.load("Sprites/Grid.png")
+spr_grid1 = pygame.image.load("Sprites/grid1.png")
+spr_grid2 = pygame.image.load("Sprites/grid2.png")
+spr_grid3 = pygame.image.load("Sprites/grid3.png")
+spr_grid4 = pygame.image.load("Sprites/grid4.png")
+spr_grid5 = pygame.image.load("Sprites/grid5.png")
+spr_grid6 = pygame.image.load("Sprites/grid6.png")
+spr_grid7 = pygame.image.load("Sprites/grid7.png")
+spr_grid8 = pygame.image.load("Sprites/grid8.png")
+spr_mine = pygame.image.load("Sprites/mine.png")
+spr_mineClicked = pygame.image.load("Sprites/mineClicked.png")
+spr_mineFalse = pygame.image.load("Sprites/mineFalse.png")
+
+
+# valeur global
+grid = []  # grille principale
+mines = []  # pos des mines
+
+
+# fonction pour dessiner du txt
+def drawText(txt, s, yOff=0): #le yOff c'est juste pour on/off l'ecriture du txt
+    screen_text = pygame.font.SysFont("Calibri", s, True).render(txt, True, (0, 0, 0))
+    rect = screen_text.get_rect()
+    rect.center = (game_width * grid_size / 2 + border, game_height * grid_size / 2 + top_border + yOff)
+    gameDisplay.blit(screen_text, rect)
+
+
+# classe grille
+class Grid:
+    def __init__(self, xGrid, yGrid, type):
+        self.xGrid = xGrid  # X pos
+        self.yGrid = yGrid  # Y pos
+        self.clicked = False  # verifie si grille est cliquée
+        self.mineClicked = False  # verifie si grille est cliquée + si mine
+        self.mineFalse = False  # le joueur a signal la mauvaise grille
+        self.flag = False  # le joueur a signalé la  grille
+        # creer rectObject pour gérer le dessin et les collisions
+        self.rect = pygame.Rect(border + self.xGrid * grid_size, top_border + self.yGrid * grid_size, grid_size, grid_size)
+        self.val = type  # valeur de la grille
+
+    def drawGrid(self):
+        # dessin grille en fonction variables bool et valeur grille
+        if self.mineFalse:
+            gameDisplay.blit(spr_mineFalse, self.rect)
+        else:
+            if self.clicked:
+                if self.val == -1:
+                    if self.mineClicked:
+                        gameDisplay.blit(spr_mineClicked, self.rect)
+                    else:
+                        gameDisplay.blit(spr_mine, self.rect)
+                else:
+                    if self.val == 0:
+                        gameDisplay.blit(spr_emptyGrid, self.rect)
+                    elif self.val == 1:
+                        gameDisplay.blit(spr_grid1, self.rect)
+                    elif self.val == 2:
+                        gameDisplay.blit(spr_grid2, self.rect)
+                    elif self.val == 3:
+                        gameDisplay.blit(spr_grid3, self.rect)
+                    elif self.val == 4:
+                        gameDisplay.blit(spr_grid4, self.rect)
+                    elif self.val == 5:
+                        gameDisplay.blit(spr_grid5, self.rect)
+                    elif self.val == 6:
+                        gameDisplay.blit(spr_grid6, self.rect)
+                    elif self.val == 7:
+                        gameDisplay.blit(spr_grid7, self.rect)
+                    elif self.val == 8:
+                        gameDisplay.blit(spr_grid8, self.rect)
+
+            else:
+                if self.flag:
+                    gameDisplay.blit(spr_flag, self.rect)
+                else:
+                    gameDisplay.blit(spr_grid, self.rect)
+
+    def revealGrid(self):
+        self.clicked = True
+        # auto montre si c 0
+        if self.val == 0:
+            for x in range(-1, 2):
+                if self.xGrid + x >= 0 and self.xGrid + x < game_width:
+                    for y in range(-1, 2):
+                        if self.yGrid + y >= 0 and self.yGrid + y < game_height:
+                            if not grid[self.yGrid + y][self.xGrid + x].clicked:
+                                grid[self.yGrid + y][self.xGrid + x].revealGrid()
+        elif self.val == -1:
+            # auto montre tte les mines si mine
+            for m in mines:
+                if not grid[m[1]][m[0]].clicked:
+                    grid[m[1]][m[0]].revealGrid()
+
+    def updateValue(self):
+        # update des valeurs qd tt trouvé
+        if self.val != -1:
+            for x in range(-1, 2):
+                if self.xGrid + x >= 0 and self.xGrid + x < game_width:
+                    for y in range(-1, 2):
+                        if self.yGrid + y >= 0 and self.yGrid + y < game_height:
+                            if grid[self.yGrid + y][self.xGrid + x].val == -1:
+                                self.val += 1
+
+
+def gameLoop():
+    gameState = "Playing"  # etat jeu savoir si il est en jeu ou non
+    mineLeft = numMine  # nbr mines restantes
+    global grid  # acces global var
+    grid = []
+    global mines
+    t = 0  # def du tp a 0
+
+    # genere de mines
+    mines = [[random.randrange(0, game_width),
+              random.randrange(0, game_height)]]
+
+    for c in range(numMine - 1):
+        pos = [random.randrange(0, game_width),
+               random.randrange(0, game_height)]
+        same = True
+        while same:
+            for i in range(len(mines)):
+                if pos == mines[i]:
+                    pos = [random.randrange(0, game_width), random.randrange(0, game_height)]
+                    break
+                if i == len(mines) - 1:
+                    same = False
+        mines.append(pos)
+
+    # genere tte la grille
+    for j in range(game_height):
+        line = []
+        for i in range(game_width):
+            if [i, j] in mines:
+                line.append(Grid(i, j, -1))
+            else:
+                line.append(Grid(i, j, 0))
+        grid.append(line)
+
+    # maj de la grille
+    for i in grid:
+        for j in i:
+            j.updateValue()
+
+    # main boucle
+    while gameState != "Exit":
+        # reset de l'ecran
+        gameDisplay.fill(bg_color)
+
+        # user entrees
+        for event in pygame.event.get():
+            # check si le joueur ferme page
+            if event.type == pygame.QUIT:
+                gameState = "Exit"
+            # check si jeu restart
+            if gameState == "Game Over" or gameState == "Win":
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        gameState = "Exit"
+                        gameLoop()
+            else:
+                if event.type == pygame.MOUSEBUTTONUP:
+                    for i in grid:
+                        for j in i:
+                            if j.rect.collidepoint(event.pos):
+                                if event.button == 1:
+                                    # si joueur clique gauche sur la grille
+                                    j.revealGrid()
+                                    # desactiv drapo
+                                    if j.flag:
+                                        mineLeft += 1
+                                        j.falg = False
+                                    # si c une mine
+                                    if j.val == -1:
+                                        gameState = "Game Over"
+                                        j.mineClicked = True
+                                elif event.button == 3:
+                                    # si joueur clique juste
+                                    if not j.clicked:
+                                        if j.flag:
+                                            j.flag = False
+                                            mineLeft += 1
+                                        else:
+                                            j.flag = True
+                                            mineLeft -= 1
+
+        # check si win
+        w = True
+        for i in grid:
+            for j in i:
+                j.drawGrid()
+                if j.val != -1 and not j.clicked:
+                    w = False
+        if w and gameState != "Exit":
+            gameState = "Win"
+
+        # dessin txt
+        if gameState != "Game Over" and gameState != "Win":
+            t += 1
+        elif gameState == "Game Over":
+            drawText("Game Over!", 50)
+            drawText("R to restart", 35, 50)
+            for i in grid:
+                for j in i:
+                    if j.flag and j.val != -1:
+                        j.mineFalse = True
+        else:
+            drawText("You WON!", 50)
+            drawText("R to restart", 35, 50)
+        # dessin temps
+        s = str(t // 15)
+        screen_text = pygame.font.SysFont("Calibri", 50).render(s, True, (0, 0, 0))
+        gameDisplay.blit(screen_text, (border, border))
+        # dessine mine restte
+        screen_text = pygame.font.SysFont("Calibri", 50).render(mineLeft.__str__(), True, (0, 0, 0))
+        gameDisplay.blit(screen_text, (display_width - border - 50, border))
+
+        pygame.display.update()  # maj ecran
+
+        timer.tick(15)  # tick fps
+
+
+gameLoop()
+pygame.quit()
+quit()
+
+#32h
